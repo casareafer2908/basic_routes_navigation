ed52a9b8b69c0789072603c43a5fef622da14ef4
Carlos Arellano Casale <40230544+casareafer2908@users.noreply.github.com>
2021-08-25 12:55:13 +0800

    First Commit
    Create base project and add basic structure + some classes to be used later

---
+++ lib/src/navigation/routes.dart
@@ -0,0 +1,31 @@
+import 'package:counter/src/ui/first_screen.dart';
+import 'package:counter/src/ui/home_screen.dart';
+import 'package:counter/src/ui/second_screen.dart';
+import 'package:counter/src/ui/third_screen.dart';
+import 'package:flutter/material.dart';
+
+class Routes {
+  static const homePage = '/';
+  static const firstPage = '/firstPage';
+  static const secondPage = '/secondPage';
+  static const thirdPage = '/thirdPage';
+
+  static Route routes(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case homePage:
+        return MaterialPageRoute(
+            builder: (context) => const HomePage(title: 'home'));
+      case firstPage:
+        return MaterialPageRoute(
+            builder: (context) => const FirstPage(title: 'First Page'));
+      case secondPage:
+        return MaterialPageRoute(
+            builder: (context) => const SecondPage(title: 'Second Page'));
+      case thirdPage:
+        return MaterialPageRoute(
+            builder: (context) => const ThirdPage(title: 'Third Page'));
+      default:
+        throw Exception('Whoops, How did i get here??!?!');
+    }
+  }
+}
